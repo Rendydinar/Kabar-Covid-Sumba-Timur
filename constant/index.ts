adf663cb03fb1f1bdee88e8bfc69541f8e4cb3f4
Waingapucoder <r3ndydinar@gmail.com>
2021-08-15 12:06:39 +0800

    Add sitemap to SEO

--- constant/index.ts
+++ constant/index.ts
@@ -1,3 +1,4 @@
 export const MESSAGE_WHATSSAPP: string = `
   Hallo Admin *Kabar Covid Sumba Timur* saya mau memberikan informasi Vaksin Covid-19 Sumba Timur. (Lanjut dengan pesan kamu)`;
 export const LINK_FEEDBACK = 'https://forms.gle/rDee3aL4oHKoQDjH7'
+export const PUBLIC_PATH = "https://kabar-covid-sumba-timur.vercel.app";
