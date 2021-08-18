ee89809b3f39aac5a9d1a1de297f7a01bf6b2f5b
Waingapucoder <r3ndydinar@gmail.com>
2021-08-18 12:36:17 +0800

    Fix BUG options filter data vaksin

--- pages/kabar-vaksin.tsx
+++ pages/kabar-vaksin.tsx
@@ -117,7 +117,6 @@ const KabarVaksin: React.FC<IProps> = (props): ReactElement => {
       });
       setListDataVaksinToShow(tempListVaksin);
     } else {
-      setSelectedFilterVaksinType('None');
       setListDataVaksinToShow(props.data);
     }
   };
