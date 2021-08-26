36773148f7215e3fcc667bd77adf193f65e5fd9a
Waingapucoder <r3ndydinar@gmail.com>
2021-08-26 13:59:56 +0800

    Add QnA content

--- components/ItemQnA/index.tsx
+++ components/ItemQnA/index.tsx
@@ -1,6 +1,6 @@
 import { Button, Collapse, IconButton, Typography } from '@material-ui/core';
 import React, { useState } from 'react';
-import { IQnA } from '../../interfaces';
+import { IQnA, IContentQnA } from '../../interfaces';
 import useStyles from './style';
 import { MdExpandMore } from 'react-icons/md';
 import { classNames } from '../../lib/classNames';
@@ -43,12 +43,97 @@ const ItemQnA: React.FC<IProps> = (props) => {
         </Typography>
       </Button>
       <Collapse in={expanded} timeout='auto' unmountOnExit>
-        <Typography className={classes.description}>
-          {props.qna.content}
-        </Typography>
-        <Typography className={classes.sumber}>
-          Sumber: {props.qna.sumber}
-        </Typography>
+        <div
+          style={{
+            padding: '10px 5px',
+          }}
+        >
+          {props.qna.type === 'general' ? (
+            <>
+              <Typography className={classes.description}>
+                {props.qna.content}
+              </Typography>
+              <Typography className={classes.sumber}>
+                Sumber: {props.qna.sumber}
+              </Typography>
+            </>
+          ) : props.qna.type === 'only-list' ? (
+            <>
+              <div>
+                {props.qna.content_list.map(
+                  (content: string, index: number) => (
+                    <div
+                      style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        gap: '8px',
+                      }}
+                      key={index}
+                    >
+                      <span
+                        style={{
+                          fontSize: '10px',
+                        }}
+                      >
+                        ⚫️
+                      </span>
+                      <Typography className={classes.description}>
+                        {content}
+                      </Typography>
+                    </div>
+                  )
+                )}
+              </div>
+              <Typography className={classes.sumber}>
+                Sumber: {props.qna.sumber}
+              </Typography>
+            </>
+          ) : props.qna.type === 'content-list-content' ? (
+            <>
+              {props.qna.content_list_content.map(
+                (content: IContentQnA, index: number) => (
+                  <div
+                    style={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      gap: '8px',
+                      marginBottom: '5px',
+                    }}
+                    key={index}
+                  >
+                    <span
+                      style={{
+                        fontSize: '10px',
+                      }}
+                    >
+                      ⚫️
+                    </span>
+                    <div>
+                      <Typography className={classes.titleListContent}>
+                        {content.title}
+                      </Typography>
+                      <Typography className={classes.description} key={index}>
+                        {content.content}
+                      </Typography>
+                    </div>
+                  </div>
+                )
+              )}
+              <Typography className={classes.sumber}>
+                Sumber: {props.qna.sumber}
+              </Typography>
+            </>
+          ) : (
+            <>
+              <Typography className={classes.description}>
+                {props.qna.content}
+              </Typography>
+              <Typography className={classes.sumber}>
+                Sumber: {props.qna.sumber}
+              </Typography>
+            </>
+          )}
+        </div>
       </Collapse>
     </div>
   );
